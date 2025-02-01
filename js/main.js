f020f7d9212240196c4a7c1ac3d56283712fc6ff
SaraKatryne <sarodrigues527@gmail.com>
2025-02-01 16:46:01 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Seleciona os elementos do DOM
+const menuIcon = document.querySelector('.menu-icon');
+const ul = document.querySelector('.ul');
+const menuImage = document.querySelector('.menu-icon img')
+
+//Define caminhos das img
+const menuOpenIcon = './img/menu.png';
+const menuCloseIcon = './img/close.png';
+
+// Adiciona o evento de click
+menuIcon.addEventListener('click',()=>{
+   
+    if (ul.classList.contains('ativo')) {
+        ul.classList.remove('ativo');
+        menuImage.src = menuOpenIcon;
+    }else{
+        ul.classList.add('ativo');
+        menuImage.src = menuCloseIcon;
+    }
+});
